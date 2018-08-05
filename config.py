aeb24e66ea3930b27e9b94c7063f1fe57a7aa89d
RameshRavone <ramesh.maran443@gmail.com>
2018-08-05 10:38:00 +0530

    Update dep versions

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 
-app_id = "com.your.app_id"
+app_id = "games.misu.greatalchemist"
 
 def can_build(plat):
     return (plat == "android")
@@ -13,10 +13,10 @@ def configure(env):
         env.android_add_gradle_classpath("com.google.gms:google-services:3.1.1")
         env.android_add_gradle_plugin("com.google.gms.google-services")
 
-        env.android_add_dependency("compile 'com.google.android.gms:play-services-auth:11.6.0'")
-        env.android_add_dependency("compile 'com.google.android.gms:play-services-games:11.6.0'")
+        env.android_add_dependency("compile 'com.google.android.gms:play-services-auth:15.0.1'")
+        env.android_add_dependency("compile 'com.google.android.gms:play-services-games:15.0.1'")
 
-	env.android_add_dependency("compile 'com.google.firebase:firebase-invites:11.6.0'")
+	env.android_add_dependency("compile 'com.google.firebase:firebase-invites:16.0.1'")
 
         env.android_add_java_dir("android");
         env.android_add_res_dir("res");
