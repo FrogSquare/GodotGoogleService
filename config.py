9c6daab5551aa89b752708c40b110e6fb453c885
Jordan Tully <jtullyoops@gmail.com>
2019-04-02 21:12:40 -0300

    Clean up and remove firebase

--- config.py
+++ config.py
@@ -23,9 +23,6 @@ app_id = "com.example.game"
 def can_build(env_plat, plat = None):
     #return False
     if plat == None:
-        #print("`GodotAds`"+RED+" master "+RESET+" branch not compatable with godot 2.X")
-        #print("Try using `GodotAds` "+GREEN+" 2.X "+RESET+" branch for Godot 2.X")
-
         if isinstance(env_plat, basestring):
             plat = env_plat
         else:
@@ -43,30 +40,24 @@ def can_build(env_plat, plat = None):
 def implement(api, support=True):
     supportv4 = "{exclude group: 'com.android.support' exclude module: 'support-v4'}"
     return "implementation('"+api+"')" + (supportv4 if support else "")
-    pass
 
 def configure(env):
     if env["platform"] == "android":
         env.android_add_maven_repository("url 'https://maven.google.com'")
         env.android_add_maven_repository("url 'https://oss.sonatype.org/content/repositories/snapshots'")
 
-        env.android_add_gradle_classpath("com.google.gms:google-services:4.1.0")
-        env.android_add_gradle_plugin("com.google.gms.google-services")
-
         env.android_add_dependency(implement("com.android.support:support-fragment:28.0.0", False))
         env.android_add_dependency(implement("com.google.android.gms:play-services-auth:16.0.1"))
         env.android_add_dependency(implement("com.google.android.gms:play-services-games:16.0.0"))
 
-	env.android_add_dependency(implement("com.google.firebase:firebase-invites:16.1.0"))
-
-        env.android_add_java_dir("android");
-        env.android_add_res_dir("res");
+        env.android_add_java_dir("android")
+        env.android_add_res_dir("res")
 
         if "frogutils" in [os.path.split(path)[1] for path in env.android_java_dirs]: pass
-        else: env.android_add_java_dir("frogutils");
+        else: env.android_add_java_dir("frogutils")
 
-        env.android_add_to_manifest("android/AndroidManifestChunk.xml");
-        env.android_add_to_permissions("android/AndroidPermissionsChunk.xml");
+        env.android_add_to_manifest("android/AndroidManifestChunk.xml")
+        env.android_add_to_permissions("android/AndroidPermissionsChunk.xml")
         env.android_add_default_config("applicationId '"+app_id+"'")
         env.disable_module()
 
